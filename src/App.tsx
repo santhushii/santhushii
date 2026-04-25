06cf06d32a0984aef967aaf363ac2214b283c6bb
Santhushie Nallaperuma <santhushie@gmail.com>
2026-04-25 18:31:58 +0530

    fix: rename Home to Portfolio and force final production build

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import { ThemeProvider } from './contexts/ThemeContext';
 import Layout from './components/Layout/Layout';
-import Home from './pages/Home';
+import Home from './pages/Portfolio';
 import LoadingScreen from './components/ui/LoadingScreen';
 
 function App() {
